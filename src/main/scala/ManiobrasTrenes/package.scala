bde20d7be9dd1456d08bd9c40baae63e91133e3e
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2023-10-08 09:31:01 -0500

    AplicarMovimientos() Implementada.

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -55,26 +55,32 @@ package object ManiobrasTrenes {
     case Uno(n) => if (n > 0) {
       (e._1 take (e._1.length - n), e._2 ++ e._1 drop (e._1.length - n), e._3)
     } else if (n < 0) {
-      (e._1 ++ e._2 take (n.abs), e._2 drop n.abs, e._3)
+      (e._1 ++ e._2 take n.abs, e._2 drop n.abs, e._3)
     } else
       (e._1, e._2, e._3)
     case Dos(n) => if (n > 0) {
       (e._1 take (e._1.length - n), e._2, e._3 ++ e._1 drop (e._1.length - n))
     } else if (n < 0) {
-      (e._1 ++ e._3 take (n.abs), e._2, e._3 drop n.abs)
+      (e._1 ++ e._3 take n.abs, e._2, e._3 drop n.abs)
     } else {
       (e._1, e._2, e._3)
     }
   }
 
-/*
+
 def aplicarMovimientos(e: Estado, movs: Maniobra): List[Estado] = {
-  List(new Estado(Nil,Nil,Nil))
+  def listaDeEstados(estado: Estado, maniobra: Maniobra): List[Estado] = {
+    maniobra match {
+      case x :: xs => aplicarMovimiento(estado, x) :: listaDeEstados(aplicarMovimiento(estado, x), xs)
+      case Nil => Nil
+    }
+  }
+  e::listaDeEstados(e,movs)
 }
 
 def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
   List(Uno(0), Dos(0))
 }
- */
+
 
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -19,10 +19,10 @@ val e4 = aplicarMovimiento1 (e3, Dos(-1))
 val e5 = aplicarMovimiento1 (e4,Uno(-2))
 val e6 = aplicarMovimiento1 ((List('a'),List('c'),List('b','d')),Dos(-2))
 val e6 = aplicarMovimiento1 ((List('a'),List('c', 'd'),List('b')),Uno(-2))
-/*
+
 aplicarMovimientos(e1, List(Uno(2), Dos(3), Dos(-1), Uno(-2), Dos(-1)))
 val e = (List('a', 'b'), List('c'), List('d'))
 aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
-definirManiobra(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a'))
- */
+//definirManiobra(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a'))
+
 
