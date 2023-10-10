4e3a5d9fff6434a75255603a0d978cc013131387
Sebastián Orrego <orrego.sebastian@correounivalle.edu.co>
2023-10-10 11:38:54 -0500

    Excepciones de carriles vacios agregadas en aplicarMovimiento

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -15,61 +15,43 @@ package object ManiobrasTrenes {
 
   type Maniobra = List[Movimiento]
 
-  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = {
-    def moverUno(n: Int, estado: Estado): Estado = {
-      if (n > 0)
-        if (n <= estado._1.length)
-          moverUno(n - 1, (estado._1.init, estado._1.last :: estado._2, estado._3))
-        else
-          moverUno(estado._1.length - 1, (estado._1.init, estado._1.last :: estado._2, estado._3))
-      else if (n < 0)
-        if (n.abs <= estado._2.length)
-          moverUno(n + 1, (estado._1 ++ List(estado._2.head), estado._2.tail, estado._3))
-        else
-          moverUno(estado._2.length - 1, (estado._1 ++ List(estado._2.head), estado._2.tail, estado._3))
-      else
-        estado
-    }
-
-    def moverDos(n: Int, estado: Estado): Estado = {
-      if (n > 0)
-        if (n <= estado._1.length)
-          moverDos(n - 1, (estado._1.init, estado._2, estado._1.last :: estado._3))
-        else
-          moverDos(estado._1.length - 1, (estado._1.init, estado._2, estado._1.last :: estado._3))
-      else if (n < 0)
-        if (n.abs <= estado._3.length)
-          moverDos(n + 1, (estado._1 ++ List(estado._3.head), estado._2, estado._3.tail))
-        else
-          moverDos(estado._3.length - 1, (estado._1 ++ List(estado._3.head), estado._2, estado._3.tail))
-      else estado
-    }
-
-    m match {
-      case Uno(n) => moverUno(n, e)
-      case Dos(n) => moverDos(n, e)
-    }
-  }
-
-  def aplicarMovimiento1(e: Estado, m: Movimiento): Estado = m match {
+  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = m match {
     case Uno(n) =>
-      if (n > 0)
-        (e._1 take (e._1.length - n), (e._1 drop (e._1.length - n)) ++ e._2, e._3)
-      else if (n < 0)
-        if (n.abs==e._2.length)
-          (e._1 ++ (e._2 take (n.abs+1)), e._2 drop n.abs, e._3)
+      if (n > 0) {
+        if (e._1.nonEmpty)
+          (e._1 take (e._1.length - n), (e._1 drop (e._1.length - n)) ++ e._2, e._3)
         else
-          ( e._1 ++ (e._2 take (n.abs)), e._2 drop n.abs, e._3)
+          (e._1, e._2, e._3)
+      } else if (n < 0)
+        if (n.abs==e._2.length)
+          if (e._2.nonEmpty)
+            (e._1 ++ (e._2 take (n.abs+1)), e._2 drop n.abs, e._3)
+          else
+            (e._1, e._2, e._3)
+        else
+          if (e._2.nonEmpty)
+            ( e._1 ++ (e._2 take (n.abs)), e._2 drop n.abs, e._3)
+          else
+            (e._1, e._2, e._3)
       else
         (e._1, e._2, e._3)
     case Dos(n) =>
       if (n > 0)
-        (e._1 take (e._1.length - n), e._2, (e._1 drop (e._1.length - n)) ++ e._3)
+        if (e._1.nonEmpty)
+          (e._1 take (e._1.length - n), e._2, (e._1 drop (e._1.length - n)) ++ e._3)
+        else
+          (e._1, e._2, e._3)
       else if (n < 0)
         if (n.abs==e._3.length)
-          (e._1++ (e._3 take (n.abs+1)), e._2, e._3 drop n.abs)
+          if (e._3.nonEmpty)
+            (e._1++ (e._3 take (n.abs+1)), e._2, e._3 drop n.abs)
+          else
+            (e._1, e._2, e._3)
         else
-          (e._1 ++ (e._3 take (n.abs)), e._2, e._3 drop n.abs)
+          if (e._3.nonEmpty)
+            (e._1 ++ (e._3 take (n.abs)), e._2, e._3 drop n.abs)
+          else
+            (e._1, e._2, e._3)
       else
         (e._1, e._2, e._3)
   }
@@ -84,58 +66,7 @@ package object ManiobrasTrenes {
     e::listaDeEstados(e,movs)
   }
 
-  def aplicarMovimientos1(e: Estado, movs: Maniobra): List[Estado] = {
-    def listaDeEstados(estado: Estado, maniobra: Maniobra): List[Estado] = {
-      maniobra match {
-        case x :: xs => aplicarMovimiento1(estado, x) :: listaDeEstados(aplicarMovimiento1(estado, x), xs)
-        case Nil => Nil
-      }
-    }
-
-    e :: listaDeEstados(e, movs)
-  }
-
   def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
-
-    def listaDeMovimientos(e: Estado, v:Vagon): List[Movimiento] = {
-      if (t2.indexOf(v) == e._1.indexOf(v)) {
-        if(e._3.nonEmpty)
-          Dos(-e._3.length) :: listaDeMovimientos(aplicarMovimiento(e, Dos(-e._3.length)), v)
-        else
-          Nil
-      }
-      else {
-        if (e._1 contains v) {
-          if (e._1.indexOf(v) == e._1.indexOf(e._1.last)) {
-            Uno(1) :: listaDeMovimientos(aplicarMovimiento(e, Uno(1)), v)
-          } else {
-            val cuantos = e._1.length - (e._1.indexOf(v) + 1)
-            Dos(cuantos) :: listaDeMovimientos(aplicarMovimiento(e, Dos(cuantos)), v)
-          }
-        }
-        else if ((e._2 contains v) && e._1.length == t2.indexOf(v)) {
-          Uno(-1) :: listaDeMovimientos(aplicarMovimiento(e, Uno(-1)), v)
-        } else {
-          val cuantos = e._1.length - t2.indexOf(v)
-          Dos(cuantos) :: listaDeMovimientos(aplicarMovimiento(e, Dos(cuantos)), v)
-        }
-      }
-    }
-
-    val estado = (t1,Nil,Nil)
-    def listarMovimientos(tren: Tren, e: Estado): Maniobra = {
-      tren match {
-        case x :: xs => {
-          val listaMovimientos = listaDeMovimientos(e, x: Vagon)
-          listaMovimientos ++ listarMovimientos(xs, aplicarMovimientos(e, listaMovimientos).last)
-        }
-        case Nil => Nil
-      }
-    }
-    listarMovimientos(t2,estado)
-  }
-
-  def definirManiobra1(t1: Tren, t2: Tren): Maniobra = {
 
     def listaDeMovimientos(e: Estado, v: Vagon): List[Movimiento] = {
       t2.indexOf(v) == e._1.indexOf(v) match {
@@ -180,4 +111,85 @@ package object ManiobrasTrenes {
 
     listarMovimientos(t2, estado)
   }
+
+  /*
+  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = {
+      def moverUno(n: Int, estado: Estado): Estado = {
+        if (n > 0)
+          if (n <= estado._1.length)
+            moverUno(n - 1, (estado._1.init, estado._1.last :: estado._2, estado._3))
+          else
+            moverUno(estado._1.length - 1, (estado._1.init, estado._1.last :: estado._2, estado._3))
+        else if (n < 0)
+          if (n.abs <= estado._2.length)
+            moverUno(n + 1, (estado._1 ++ List(estado._2.head), estado._2.tail, estado._3))
+          else
+            moverUno(estado._2.length - 1, (estado._1 ++ List(estado._2.head), estado._2.tail, estado._3))
+        else
+          estado
+      }
+
+      def moverDos(n: Int, estado: Estado): Estado = {
+        if (n > 0)
+          if (n <= estado._1.length)
+            moverDos(n - 1, (estado._1.init, estado._2, estado._1.last :: estado._3))
+          else
+            moverDos(estado._1.length - 1, (estado._1.init, estado._2, estado._1.last :: estado._3))
+        else if (n < 0)
+          if (n.abs <= estado._3.length)
+            moverDos(n + 1, (estado._1 ++ List(estado._3.head), estado._2, estado._3.tail))
+          else
+            moverDos(estado._3.length - 1, (estado._1 ++ List(estado._3.head), estado._2, estado._3.tail))
+        else estado
+      }
+
+      m match {
+        case Uno(n) => moverUno(n, e)
+        case Dos(n) => moverDos(n, e)
+      }
+    }
+   */
+
+  /*
+    def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
+
+      def listaDeMovimientos(e: Estado, v:Vagon): List[Movimiento] = {
+        if (t2.indexOf(v) == e._1.indexOf(v)) {
+          if(e._3.nonEmpty)
+            Dos(-e._3.length) :: listaDeMovimientos(aplicarMovimiento(e, Dos(-e._3.length)), v)
+          else
+            Nil
+        }
+        else {
+          if (e._1 contains v) {
+            if (e._1.indexOf(v) == e._1.indexOf(e._1.last)) {
+              Uno(1) :: listaDeMovimientos(aplicarMovimiento(e, Uno(1)), v)
+            } else {
+              val cuantos = e._1.length - (e._1.indexOf(v) + 1)
+              Dos(cuantos) :: listaDeMovimientos(aplicarMovimiento(e, Dos(cuantos)), v)
+            }
+          }
+          else if ((e._2 contains v) && e._1.length == t2.indexOf(v)) {
+            Uno(-1) :: listaDeMovimientos(aplicarMovimiento(e, Uno(-1)), v)
+          } else {
+            val cuantos = e._1.length - t2.indexOf(v)
+            Dos(cuantos) :: listaDeMovimientos(aplicarMovimiento(e, Dos(cuantos)), v)
+          }
+        }
+      }
+
+      val estado = (t1,Nil,Nil)
+      def listarMovimientos(tren: Tren, e: Estado): Maniobra = {
+        tren match {
+          case x :: xs => {
+            val listaMovimientos = listaDeMovimientos(e, x: Vagon)
+            listaMovimientos ++ listarMovimientos(xs, aplicarMovimientos(e, listaMovimientos).last)
+          }
+          case Nil => Nil
+        }
+      }
+      listarMovimientos(t2,estado)
+    }
+  */
+
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -12,26 +12,22 @@ val e2= aplicarMovimiento (e1,Uno(2))
 val e3 = aplicarMovimiento (e2,Dos(3))
 val e4 = aplicarMovimiento (e3, Dos(-1))
 val e5 = aplicarMovimiento (e4,Uno(-2))
+val e6 = aplicarMovimiento (e5,Dos(-1))
 
-val e2= aplicarMovimiento1 (e1,Uno(2))
-val e3 = aplicarMovimiento1 (e2,Dos(3))
-val e4 = aplicarMovimiento1 (e3, Dos(-1))
-val e5 = aplicarMovimiento1 (e4,Uno(-2))
+val e = (List('a','b'),List('c'), List('d'))
+aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
 
-aplicarMovimientos(e1, List(Uno(2), Dos(3), Dos(-1), Uno(-2), Dos(-1)))
+val m1 = (List(1, 2, 3, 4, 5), Nil, Nil)
+aplicarMovimientos(m1, List(Uno(2), Dos(3), Dos(-1), Uno(-2), Dos(-1)))
+aplicarMovimientos(m1, List(Uno(5), Uno(-1), Dos(0), Dos(4), Uno(3)))
+aplicarMovimientos(m1, List(Dos(3), Dos(-2), Uno(1), Uno(4), Dos(2)))
+aplicarMovimientos(m1, List(Dos(0), Uno(6), Dos(2), Dos(-1), Uno(-1)))
 
 val e = (List('a', 'b'), List('c'), List('d'))
 aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
-aplicarMovimientos1(e, List(Uno(1), Dos(1), Uno(-2)))
 
 definirManiobra(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a'))
 aplicarMovimientos((List('a', 'b', 'c', 'd'),Nil,Nil),definirManiobra(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a')))
 
 definirManiobra(List(1, 2, 3, 4, 5) , List(5, 4, 3, 2, 1))
 aplicarMovimientos((List(1, 2, 3, 4, 5),Nil,Nil),definirManiobra(List(1, 2, 3, 4, 5) , List(5, 4, 3, 2, 1)))
-
-definirManiobra1(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a'))
-aplicarMovimientos((List('a', 'b', 'c', 'd'),Nil,Nil),definirManiobra1(List('a', 'b', 'c', 'd') , List('d', 'b', 'c', 'a')))
-
-definirManiobra1(List(1, 2, 3, 4, 5) , List(5, 4, 3, 2, 1))
-aplicarMovimientos((List(1, 2, 3, 4, 5),Nil,Nil),definirManiobra1(List(1, 2, 3, 4, 5) , List(5, 4, 3, 2, 1)))
